367144e9d26b633ca34174b5dbcae4f746c04602
Xinyiqi <shinichi_watanabe@dignicate.com>
2024-06-30 15:09:57 +0900

    Create project.

---
+++ zero-2024-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zero-2024-swift
+//
+//  Created by Xinyiqi on 2024/06/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zero-2024-swift/zero_2024_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  zero_2024_swiftApp.swift
+//  zero-2024-swift
+//
+//  Created by Xinyiqi on 2024/06/30.
+//
+
+import SwiftUI
+
+@main
+struct zero_2024_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
